db53bf85deaa6a89eb19d7ddecacd9e8622a6e47
adamk <adam.klamrowski.ak@gmail.com>
2025-08-27 12:50:31 +0200

    tests(poczta-polska): moved poczta_polska tests to module

---
+++ tests/unit/modules/poczta_polska/sender/SenderOptionsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\postal\tests\unit\modules\poczta_polska\sender;
+
+use Codeception\Test\Unit;
+use UnitTester;
+
+/**
+ * @property UnitTester $tester
+ */
+
+class SenderOptionsTest extends Unit
+{
+
+}
