6d0c2a7e2ee1a07b9312817205e4a3a8f4fa022f
adamk <adam.klamrowski.ak@gmail.com>
2025-09-03 13:11:00 +0200

    refactor: deleted namespaces

--- src/migrations/m250725_112007_init_shipment.php
+++ src/migrations/m250725_112007_init_shipment.php
@@ -1,9 +1,6 @@
 <?php
 
-namespace XOzymandias\Yii2Postal\migrations;
-
 use XOzymandias\Yii2Postal\ModuleEnsureTrait;
-use XOzymandias\Yii2Postal\tests\migrations\Exception;
 use yii\base\InvalidConfigException;
 use yii\db\Migration;
 
@@ -33,9 +30,7 @@ class m250725_112007_init_shipment extends Migration
         }
     }
 
-    /**
-     ** @throws Exception
-     */
+
     public function safeUp(): void
     {
         $this->createTable('{{%shipment_content}}', [
@@ -51,7 +46,7 @@ class m250725_112007_init_shipment extends Migration
             'postal_code' => $this->string(10)->notNull(),
             'city' => $this->string(60)->notNull(),
             'country' => $this->char(2)->notNull()->defaultValue('PL'),
-            'option' => $this->char(3)->notNull(), #@todo: remove this
+            'default_direction' => $this->string(4)->null(),
             'street' => $this->string(60)->null(),
             'apartment_number' => $this->string(10)->null(),
             'city_id' => $this->integer()->null(),
