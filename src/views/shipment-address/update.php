96a6d9a01c07707cef637132ec778fca034ad726
adamk <adam.klamrowski.ak@gmail.com>
2025-09-04 12:33:52 +0200

    refactor: migrate update view to AddressForm and fix breadcrumbs

--- src/views/shipment-address/update.php
+++ src/views/shipment-address/update.php
@@ -4,13 +4,13 @@ use XOzymandias\Yii2Postal\Module;
 use yii\helpers\Html;
 
 /** @var yii\web\View $this */
-/** @var XOzymandias\Yii2Postal\models\ShipmentAddress $model */
+/** @var XOzymandias\Yii2Postal\forms\AddressForm $model */
 
 $this->title = Module::t('common', 'Update Shipment Address: {name}', [
     'name' => $model->name,
 ]);
 $this->params['breadcrumbs'][] = ['label' => Module::t('common', 'Shipment Addresses'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->getModel()->id]];
 $this->params['breadcrumbs'][] = Module::t('common', 'Update');
 ?>
 <div class="shipment-address-update">
