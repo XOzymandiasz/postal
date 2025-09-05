51ac1ff7ac14089d0beb36eda73065aff1b6f873
adamk <adam.klamrowski.ak@gmail.com>
2025-09-05 10:25:52 +0200

    test: added new shipment address links to fixtures

--- tests/_data/shipment_address_link.php
+++ tests/_data/shipment_address_link.php
@@ -3,14 +3,24 @@
 use XOzymandias\Yii2Postal\models\ShipmentDirectionInterface;
 
 return [
-    'sender_link' => [
+    'sender_link_IN_PP' => [
         'shipment_id' => 1,
         'address_id' => 1,
         'type' => ShipmentDirectionInterface::DIRECTION_IN,
     ],
-    'receiver_link' => [
+    'receiver_link_IN_PP' => [
         'shipment_id' => 1,
         'address_id' => 2,
+        'type' => ShipmentDirectionInterface::DIRECTION_OUT,
+    ],
+    'sender_link_OUT_PP' => [
+        'shipment_id' => 2,
+        'address_id' => 3,
+        'type' => ShipmentDirectionInterface::DIRECTION_IN,
+    ],
+    'receiver_link_OUT_PP' => [
+        'shipment_id' => 2,
+        'address_id' => 4,
         'type' => ShipmentDirectionInterface::DIRECTION_OUT,
     ],
 ];
