ccae922c1e16031548a3bc1ffc4646f1c30b01e1
adamk <adam.klamrowski.ak@gmail.com>
2025-09-25 11:37:01 +0200

    feat: added creation url for delete and update

--- src/modules/poczta_polska/components/PocztaPolskaShipmentUrlComponent.php
+++ src/modules/poczta_polska/components/PocztaPolskaShipmentUrlComponent.php
@@ -9,8 +9,12 @@ use yii\helpers\Url;
 class PocztaPolskaShipmentUrlComponent extends Component
 {
     public string $createInRoute = 'shipment/create-in';
+    public string $updateRoute = 'shipment/update';
+    public string $deleteRoute = 'shipment/delete';
+
 
     public string $paramBufferId = 'bufferId';
+    public int $paramId = 0;
     public ?string $moduleId = null;
 
     public function getCreateInURL(int $bufferId, array $params = []): ?string
@@ -21,6 +25,26 @@ class PocztaPolskaShipmentUrlComponent extends Component
 
         $params[$this->paramBufferId] = $bufferId;
         return Url::to([$this->getFullRoute($this->getParentModuleId(), $this->createInRoute)] + $params);
+    }
+
+    public function getDeleteURL(int $id, array $params = []): ?string
+    {
+        if($this->moduleId == null){
+            throw new InvalidConfigException('moduleId is required');
+        }
+
+        $params[$this->paramId] = $id;
+        return Url::to([$this->getFullRoute($this->getParentModuleId(), $this->deleteRoute)] + $params);
+    }
+
+    public function getUpdateURL(int $id, array $params = []): ?string
+    {
+        if($this->moduleId == null){
+            throw new InvalidConfigException('moduleId is required');
+        }
+
+        $params[$this->paramId] = $id;
+        return Url::to([$this->getFullRoute($this->getParentModuleId(), $this->updateRoute)] + $params);
     }
 
     private function getFullRoute(string $moduleId, string $route): string
