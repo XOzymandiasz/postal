e3ae664b7274b0cb0c4eb726c195d5361a86ea62
adamk <adam.klamrowski.ak@gmail.com>
2025-09-29 16:09:11 +0200

    chore: add widgets

--- src/views/shipment/create.php
+++ src/views/shipment/create.php
@@ -3,11 +3,12 @@
 use XOzymandias\Yii2Postal\forms\ShipmentForm;
 use XOzymandias\Yii2Postal\Module;
 use yii\helpers\Html;
+use XOzymandias\Yii2Postal\widgets\ShipmentFormWidget;
 
 /** @var yii\web\View $this */
 /** @var ShipmentForm $model */
 
-$this->title = Module::t('common', 'Create Postal Shipment');
+$this->title = $model->isInScenario() ? Module::t('postal', 'Create Shipment - IN') : Module::t('postal', 'Create Shipment - OUT');
 $this->params['breadcrumbs'][] = ['label' => Module::t('common', 'Postal Shipments'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -15,8 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
-
+	<?= ShipmentFormWidget::widget([
+			'model' => $model,
+		]) ?>
 </div>
