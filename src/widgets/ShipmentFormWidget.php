016e7d16a9550dda0ff9e072d771a10415fbd51e
adamk <adam.klamrowski.ak@gmail.com>
2025-10-07 18:47:13 +0200

    feat: Enhance ShipmentFormWidget to support configurable fields and ActiveForm integration

--- src/widgets/ShipmentFormWidget.php
+++ src/widgets/ShipmentFormWidget.php
@@ -4,16 +4,25 @@ namespace XOzymandias\Yii2Postal\widgets;
 
 use XOzymandias\Yii2Postal\forms\ShipmentForm;
 use yii\base\Widget;
+use yii\widgets\ActiveForm;
 
 class ShipmentFormWidget extends Widget {
 
 	public ShipmentForm $model;
 	public array $options = [];
+	public ?ActiveForm $form = null;
+	public bool $withProviderField = true;
+	public bool $withSenderField = true;
+	public bool $withReceiverField = true;
 
 	public function run(): string {
 		return $this->render('_form', [
 			'model' => $this->model,
 			'options' => $this->options,
+			'form' => $this->form,
+			'withProviderField' => $this->withProviderField,
+			'withSenderField' => $this->withSenderField,
+			'withReceiverField' => $this->withReceiverField,
 		]);
 	}
 	public function getViewPath(): string
