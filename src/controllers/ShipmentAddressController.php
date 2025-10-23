b08304143d80b10e9e201981f1403645904d857f
adamk <adam.klamrowski.ak@gmail.com>
2025-10-23 14:18:32 +0200

    style: deleted whitespaces

--- src/controllers/ShipmentAddressController.php
+++ src/controllers/ShipmentAddressController.php
@@ -20,180 +20,180 @@ use yii\web\Response;
  */
 class ShipmentAddressController extends Controller
 {
-    public int $minLengthAddressListQuery;
+	public int $minLengthAddressListQuery;
 
-    public function behaviors(): array
-    {
-        return array_merge(
-            parent::behaviors(),
-            [
-                'verbs' => [
-                    'class' => VerbFilter::class,
-                    'actions' => [
-                        'delete' => ['POST'],
-                    ],
-                ],
-                'access' => [
-                    'class' => AccessControl::class,
-                    'only' => ['create', 'update', 'delete'],
-                    'rules' => [
-                        [
-                            'actions' => ['create', 'update', 'delete'],
-                            'allow' => true,
-                            'roles' => ['@'],
-                        ]
-                    ],
-                ]
-            ]
-        );
-    }
+	public function behaviors(): array
+	{
+		return array_merge(
+			parent::behaviors(),
+			[
+				'verbs' => [
+					'class' => VerbFilter::class,
+					'actions' => [
+						'delete' => ['POST'],
+					],
+				],
+				'access' => [
+					'class' => AccessControl::class,
+					'only' => ['create', 'update', 'delete'],
+					'rules' => [
+						[
+							'actions' => ['create', 'update', 'delete'],
+							'allow' => true,
+							'roles' => ['@'],
+						]
+					],
+				]
+			]
+		);
+	}
 
-    public function init(): void
-    {
-        parent::init();
-        $this->minLengthAddressListQuery = $this->module->minLengthAddressListQuery;
-    }
+	public function init(): void
+	{
+		parent::init();
+		$this->minLengthAddressListQuery = $this->module->minLengthAddressListQuery;
+	}
 
-    /**
-     * @return string
-     */
-    public function actionIndex(): string
-    {
-        $searchModel = new ShipmentAddressPostSearch();
-        $dataProvider = $searchModel->search($this->request->queryParams);
+	/**
+	 * @return string
+	 */
+	public function actionIndex(): string
+	{
+		$searchModel = new ShipmentAddressPostSearch();
+		$dataProvider = $searchModel->search($this->request->queryParams);
 
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
-    }
+		return $this->render('index', [
+			'searchModel' => $searchModel,
+			'dataProvider' => $dataProvider,
+		]);
+	}
 
-    /**
-     * @throws NotFoundHttpException
-     */
-    public function actionView(int $id): string
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
+	/**
+	 * @throws NotFoundHttpException
+	 */
+	public function actionView(int $id): string
+	{
+		return $this->render('view', [
+			'model' => $this->findModel($id),
+		]);
+	}
 
-    /**
-     * @throws Exception
-     */
-    public function actionCreate(): Response|string
-    {
-        $model = new AddressForm();
+	/**
+	 * @throws Exception
+	 */
+	public function actionCreate(): Response|string
+	{
+		$model = new AddressForm();
 
-        if ($model->load($this->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->getModel()->id]);
-        }
+		if ($model->load($this->request->post()) && !$model->isAddressAlreadyExist() && $model->save()) {
+			return $this->redirect(['view', 'id' => $model->getModel()->id]);
+		}
 
-        return $this->render('create', [
-            'model' => $model,
-        ]);
-    }
+		return $this->render('create', [
+			'model' => $model,
+		]);
+	}
 
-    /**
-     * @throws Exception
-     * @throws NotFoundHttpException
-     */
-    public function actionUpdate(int $id): Response|string
-    {
-        $model = new AddressForm();
-        $model->setModel($this->findModel($id));
+	/**
+	 * @throws Exception
+	 * @throws NotFoundHttpException
+	 */
+	public function actionUpdate(int $id): Response|string
+	{
+		$model = new AddressForm();
+		$model->setModel($this->findModel($id));
 
-        if ($model->default_role = ShipmentAddress::ROLE_BOTH){
-            $model->isSender = $model->isReceiver = true;
-        } elseif ($model->default_role = ShipmentAddress::ROLE_SENDER){
-            $model->isSender = true;
-        } elseif ($model->default_role = ShipmentAddress::ROLE_RECEIVER){
-            $model->isReceiver = true;
-        }
+		if ($model->default_role = ShipmentAddress::ROLE_BOTH){
+			$model->isSender = $model->isReceiver = true;
+		} elseif ($model->default_role = ShipmentAddress::ROLE_SENDER){
+			$model->isSender = true;
+		} elseif ($model->default_role = ShipmentAddress::ROLE_RECEIVER){
+			$model->isReceiver = true;
+		}
 
-        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->getModel()->id]);
-        }
+		if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+			return $this->redirect(['view', 'id' => $model->getModel()->id]);
+		}
 
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
+		return $this->render('update', [
+			'model' => $model,
+		]);
+	}
 
-    /**
-     * @throws StaleObjectException
-     * @throws Throwable
-     * @throws NotFoundHttpException
-     */
-    public function actionDelete(int $id): Response
-    {
-        $this->findModel($id)->delete();
+	/**
+	 * @throws StaleObjectException
+	 * @throws Throwable
+	 * @throws NotFoundHttpException
+	 */
+	public function actionDelete(int $id): Response
+	{
+		$this->findModel($id)->delete();
 
-        return $this->redirect(['index']);
-    }
+		return $this->redirect(['index']);
+	}
 
-    public function actionList(?string $query = null, ?string $role = null, int $id = 0): Response
-    {
-        $data = ['result' => []];
+	public function actionList(?string $query = null, ?string $role = null, int $id = 0): Response
+	{
+		$data = ['result' => []];
 
-        if ($query !== null && strlen($query) >= $this->minLengthAddressListQuery) {
-            $models = ShipmentAddress::find()
-                ->filterByFullAddress($query)
-                ->all();
-            $address = [];
-            foreach ($models as $model) {
-                $address[] = $this->parseAddress($model);
-            }
-            $data['result'] = $address;
-        } elseif ($id > 0) {
-            $model = ShipmentAddress::find()
-                ->where(['id' => $id])
-                ->one();
+		if ($query !== null && strlen($query) >= $this->minLengthAddressListQuery) {
+			$models = ShipmentAddress::find()
+				->filterByFullAddress($query)
+				->all();
+			$address = [];
+			foreach ($models as $model) {
+				$address[] = $this->parseAddress($model);
+			}
+			$data['result'] = $address;
+		} elseif ($id > 0) {
+			$model = ShipmentAddress::find()
+				->where(['id' => $id])
+				->one();
 
-            if ($model) {
-                $data['result'] = $this->parseAddress($model);
-            }
-        } elseif ($role !== null) {
-            $models = [];
-            if ($role === ShipmentAddress::ROLE_SENDER) {
-                $models = ShipmentAddress::find()
-                    ->defaultOutDirection()
-                    ->all();
-            } elseif ($role === ShipmentAddress::ROLE_RECEIVER) {
-                $models = ShipmentAddress::find()
-                    ->defaultInDirection()
-                    ->all();
-            }
+			if ($model) {
+				$data['result'] = $this->parseAddress($model);
+			}
+		} elseif ($role !== null) {
+			$models = [];
+			if ($role === ShipmentAddress::ROLE_SENDER) {
+				$models = ShipmentAddress::find()
+					->defaultOutDirection()
+					->all();
+			} elseif ($role === ShipmentAddress::ROLE_RECEIVER) {
+				$models = ShipmentAddress::find()
+					->defaultInDirection()
+					->all();
+			}
 
-            $address = [];
-            foreach ($models as $model) {
-                $address[] = $this->parseAddress($model);
-            }
-            $data['result'] = $address;
-        }
+			$address = [];
+			foreach ($models as $model) {
+				$address[] = $this->parseAddress($model);
+			}
+			$data['result'] = $address;
+		}
 
-        return $this->asJson($data);
-    }
+		return $this->asJson($data);
+	}
 
-    /**
-     * @throws NotFoundHttpException
-     */
-    protected function findModel($id): ?ShipmentAddress
-    {
-        if (($model = ShipmentAddress::findOne(['id' => $id])) !== null) {
-            return $model;
-        }
+	/**
+	 * @throws NotFoundHttpException
+	 */
+	protected function findModel($id): ?ShipmentAddress
+	{
+		if (($model = ShipmentAddress::findOne(['id' => $id])) !== null) {
+			return $model;
+		}
 
-        throw new NotFoundHttpException(Module::t('postal', 'The requested page does not exist.'));
-    }
+		throw new NotFoundHttpException(Module::t('postal', 'The requested page does not exist.'));
+	}
 
-    protected function parseAddress(ShipmentAddress $model): array
-    {
-        return [
-            'id' => $model->id,
-            'text' => $model->getFullInfo(),
-        ];
-    }
+	protected function parseAddress(ShipmentAddress $model): array
+	{
+		return [
+			'id' => $model->id,
+			'text' => $model->getFullInfo(),
+		];
+	}
 
 
 }
