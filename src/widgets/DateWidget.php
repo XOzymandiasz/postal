2cd9be896938ef70d7ecf513d52c90ae5d133eb5
adamk <adam.klamrowski.ak@gmail.com>
2025-10-24 15:20:22 +0200

    feat: add base format

--- src/widgets/DateWidget.php
+++ src/widgets/DateWidget.php
@@ -5,5 +5,18 @@ namespace XOzymandias\Yii2Postal\widgets;
 use kartik\date\DatePicker as BaseDateWidget;
 
 class DateWidget extends BaseDateWidget {
+	public function init(): void
+	{
+		$this->pluginOptions = array_merge([
+			'autoclose' => true,
+			'format' => 'yyyy-mm-dd',
+			'todayHighlight' => true,
+		], $this->pluginOptions ?? []);
 
+		$this->options = array_merge([
+			'placeholder' => 'YYYY-MM-DD',
+		], $this->options ?? []);
+
+		parent::init();
+	}
 }
