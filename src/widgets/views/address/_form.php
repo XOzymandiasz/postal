61e0dce4886eaafdf01d3ef12b6a52245a6dcb63
adamk <adam.klamrowski.ak@gmail.com>
2025-10-25 17:48:35 +0200

    refactor: make fields smaller

--- src/widgets/views/address/_form.php
+++ src/widgets/views/address/_form.php
@@ -25,8 +25,7 @@ $formWasNull = $form === null;
 	<div class="row">
 		<div class="col-12 col-sm-8 col-md-6 col-lg-6">
 			<?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-		</div>
-	</div>
+
 
 	<div class="row">
 		<div class="col-6 col-sm-4 col-md-2 col-lg-2">
@@ -44,10 +43,10 @@ $formWasNull = $form === null;
 		<div class="col-12 col-sm-6 col-md-6 col-lg-4">
 			<?= $form->field($model, 'street')->textInput(['maxlength' => true]) ?>
 		</div>
-		<div class="col-6 col-sm-3 col-md-3 col-lg-3">
+		<div class="col-6 col-sm-3 col-md-2 col-lg-1">
 			<?= $form->field($model, 'house_number')->textInput(['maxlength' => true]) ?>
 		</div>
-		<div class="col-6 col-sm-4 col-md-4 col-lg-2">
+		<div class="col-6 col-sm-4 col-md-2 col-lg-1">
 			<?= $form->field($model, 'apartment_number')->textInput(['maxlength' => true]) ?>
 		</div>
 	</div>
