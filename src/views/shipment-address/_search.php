be43b1ee4d1b09c1923c6dca6745eac37303c094
adamk <adam.klamrowski.ak@gmail.com>
2025-10-25 17:48:09 +0200

    feat: improved search view

--- src/views/shipment-address/_search.php
+++ src/views/shipment-address/_search.php
@@ -1,42 +1,53 @@
 <?php
 
-use XOzymandias\Yii2Postal\Module;
-use yii\helpers\Html;
+use XOzymandias\Yii2Postal\models\search\ShipmentAddressPostSearch;
 use yii\widgets\ActiveForm;
+use yii\web\View;
 
-/** @var yii\web\View $this */
-/** @var \XOzymandias\Yii2Postal\models\search\ShipmentAddressPostSearch $model */
-/** @var yii\widgets\ActiveForm $form */
+/** @var View $this */
+/** @var ShipmentAddressPostSearch $model */
+/** @var ActiveForm $form */
 ?>
 
-<div class="shipment-address-search">
+	<div class="shipment-address-search">
 
-    <?php $form = ActiveForm::begin([
-        'action' => ['index'],
-        'method' => 'get',
-    ]); ?>
+		<?php $form = ActiveForm::begin([
+			'action' => ['index'],
+			'method' => 'get',
+		]); ?>
 
-    <?= $form->field($model, 'id') ?>
+		<?php //$form->field($model, 'id') ?>
 
-    <?= $form->field($model, 'name') ?>
+		<div class="row">
+			<div class="col-12 col-sm-8 col-md-6 col-lg-6">
+				<?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+			</div>
+		</div>
 
-    <?= $form->field($model, 'street') ?>
+		<div class="row">
+			<div class="col-12 col-sm-6 col-md-6 col-lg-4">
+				<?= $form->field($model, 'street')->textInput(['maxlength' => true]) ?>
+			</div>
+			<div class="col-6 col-sm-3 col-md-2 col-lg-1">
+				<?= $form->field($model, 'house_number')->textInput(['maxlength' => true]) ?>
+			</div>
+			<div class="col-6 col-sm-4 col-md-2 col-lg-1">
+				<?= $form->field($model, 'apartment_number')->textInput(['maxlength' => true]) ?>
+			</div>
+		</div>
 
-    <?= $form->field($model, 'house_number') ?>
+		<div class="row">
+			<div class="col-6 col-sm-4 col-md-2 col-lg-2">
+				<?= $form->field($model, 'postal_code')->textInput(['maxlength' => true]) ?>
+			</div>
+			<div class="col-6 col-sm-7 col-md-6 col-lg-5">
+				<?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
+			</div>
+		</div>
 
-    <?= $form->field($model, 'apartment_number') ?>
+		<?php // echo $form->field($model, 'country') ?>
 
-    <?php // echo $form->field($model, 'postal_code') ?>
+		<?php ActiveForm::end(); ?>
 
-    <?php // echo $form->field($model, 'city') ?>
-
-    <?php // echo $form->field($model, 'country') ?>
-
-    <div class="form-group">
-        <?= Html::submitButton(Module::t('common', 'Search'), ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton(Module::t('common', 'Reset'), ['class' => 'btn btn-outline-secondary']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
-</div>
+	</div>
+<?php
