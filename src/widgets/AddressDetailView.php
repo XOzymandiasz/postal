683bc78ad97522791b2376a95d73641bfcf38162
adamk <adam.klamrowski.ak@gmail.com>
2025-10-27 15:26:16 +0100

    feat: create widget for addresses

---
+++ src/widgets/AddressDetailView.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace XOzymandias\Yii2Postal\widgets;
+
+use XOzymandias\Yii2Postal\models\ShipmentAddress;
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\widgets\DetailView;
+
+/**
+ * @property ShipmentAddress $model
+ */
+
+class AddressDetailView extends DetailView {
+
+	public ?string $role = null;
+
+	public function init(): void {
+
+		if (!$this->model instanceof ShipmentAddress) {
+			throw new InvalidConfigException(sprintf(
+				'Property "model" must be an instance of %s, %s given.',
+				ShipmentAddress::class,
+				is_object($this->model) ? get_class($this->model) : gettype($this->model)
+			));
+		}
+
+		if ($this->role === null) {
+			$this->role = Yii::t('postal', 'Address');
+		}
+
+		if (empty($this->options)) {
+			$this->options = $this->defaultOptions();
+		}
+
+		if (empty($this->attributes)){
+			$this->attributes = $this->defaultAttributes();
+		}
+		parent::init();
+	}
+
+	public function defaultOptions(): array {
+		return [
+			'class' => 'table table-striped table-bordered detail-view',
+			'style' => 'table-layout:fixed;width:100%;',
+		];
+	}
+
+	public function defaultAttributes(): array {
+		return [
+			[
+				'label' => Yii::t('postal', 'Role'),
+				'value' => $this->role,
+				'format' => 'text',
+			],
+			[
+				'attribute' => 'name',
+				'label' => Yii::t('postal', 'Name'),
+				'value' => $this->model->name,
+				'format' => 'text',
+			],
+			[
+				'attribute' => 'postal_code',
+				'label' => Yii::t('postal', 'Postal Code'),
+				'value' => $this->model->postal_code,
+				'format' => 'text',
+			],
+			[
+				'attribute' => 'city',
+				'label' => Yii::t('postal', 'City'),
+				'value' => $this->model->city,
+				'format' => 'text',
+			],
+			[
+				'attribute' => 'street',
+				'label' => Yii::t('postal', 'Street'),
+				'value' => $this->model->street,
+				'visible' => isset($this->model->street),
+				'format' => 'text',
+			],
+			[
+				'attribute' => 'house_number',
+				'label' => Yii::t('postal', 'House Number'),
+				'value' => $this->model->house_number,
+				'format' => 'text',
+			],
+			[
+				'attribute' => 'apartment_number',
+				'label' => Yii::t('postal', 'Apartment Number'),
+				'value' => $this->model->apartment_number,
+				'format' => 'text',
+				'visible' => isset($this->model->apartment_number),
+			],
+		];
+	}
+
+
+}
