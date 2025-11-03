4eba5b9994bec0129c59778ce35464e9fd6ac930
adamk <adam.klamrowski.ak@gmail.com>
2025-11-03 17:20:03 +0100

    fix: correct fields sizes

--- src/widgets/views/address/_form.php
+++ src/widgets/views/address/_form.php
@@ -1,90 +1,95 @@
 <?php
 
+use XOzymandias\Yii2Postal\forms\ShipmentForm;
+use XOzymandias\Yii2Postal\models\ShipmentAddress;
 use XOzymandias\Yii2Postal\Module;
+use XOzymandias\Yii2Postal\widgets\ShipmentAddressWidget;
+use XOzymandias\Yii2Postal\widgets\DateTimeWidget;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\select2\Select2;
 
 /** @var yii\web\View $this */
-/** @var XOzymandias\Yii2Postal\models\ShipmentAddress $model */
-/** @var yii\widgets\ActiveForm|null $form */
+/** @var ShipmentForm $model */
+/** @var yii\widgets\ActiveForm $form */
+/** @var bool $withProviderField */
+/** @var bool $withSenderField */
+/** @var bool $withReceiverField */
 
-$formWasNull = $form === null;
 ?>
 
-<div class="shipment-address-form">
+<div class="postal-shipment-form">
 
-
-	<?php
-	if ($formWasNull) {
-		$form = ActiveForm::begin([
-			'id' => 'shipment-address-form',
-		]);
-	}
-	?>
+	<?php $form = ActiveForm::begin([
+		'id' => 'shipment-form',
+	]); ?>
 
 	<div class="row">
-		<div class="col-sm-12 col-md-10 col-lg-9">
-			<?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+		<div class="col-sm-5 col-md-4 col-lg-3">
+			<?= $form->field($model, 'number')->textInput(['maxlength' => true, 'autofocus' => true]) ?>
 		</div>
-	</div>
-	<div class="row">
-		<div class="col-sm-4 col-md-2 col-lg-2">
-			<?= $form->field($model, 'postal_code', [
-				'labelOptions' => ['style' => 'white-space: nowrap;']
-			])->textInput(['maxlength' => true]) ?>
-		</div>
-		<div class="col-sm-5 col-md-6 col-lg-5">
-			<?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
-		</div>
-		<div class="col-sm-3 col-md-2 col-lg-2">
-			<?= $form->field($model, 'country')->textInput(['maxlength' => true]) ?>
+
+		<div class="col-sm-4 col-md-3 col-lg-2">
+			<?= $withProviderField ? $form->field($model, 'provider')->widget(Select2::class, [
+				'data' => $model::getProvidersNames(),
+				'options' => ['placeholder' => Module::t('postal', 'Choose provider')],
+			]) : '' ?>
 		</div>
 	</div>
 
 	<div class="row">
-		<div class="col-sm-6 col-md-6 col-lg-5">
-			<?= $form->field($model, 'street')->textInput(['maxlength' => true]) ?>
-		</div>
-		<div class="col-sm-3 col-md-2 col-lg-2">
-			<?= $form->field($model, 'house_number', [
-				'labelOptions' => ['style' => 'white-space: nowrap;']
-			])->textInput(['maxlength' => true]) ?>
-		</div>
-		<div class="col-sm-3 col-md-2 col-lg-2">
-			<?= $form->field($model, 'apartment_number', [
-				'labelOptions' => ['style' => 'white-space: nowrap;']
-			])->textInput(['maxlength' => true]) ?>
+		<div class="col-sm-9 col-md-7 col-lg-5">
+			<?= $form->field($model, 'content_id')->widget(Select2::class, [
+				'data' => $model->getContentNames(),
+				'options' => ['placeholder' => Module::t('postal', 'Choose content')],
+				'pluginOptions' => ['tags' => true],
+			]) ?>
 		</div>
 	</div>
 
 	<div class="row">
-		<div class="col-sm-12 col-md-10 col-lg-9">
-			<?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+		<div class="col-sm-9 col-md-7 col-lg-5">
+			<?= $withSenderField ? $form->field($model, 'sender_id')->widget(ShipmentAddressWidget::class, [
+				'role' => ShipmentAddress::ROLE_SENDER,
+				'options' => ['placeholder' => Module::t('postal', 'Choose Sender')],
+				'addressProperty' => 'senderAddress',
+			])->hint(
+				Html::a(
+					Module::t('postal', 'Create Sender Address'),
+					['shipment-address/create']
+				)
+			) : '' ?>
 		</div>
 	</div>
 
 	<div class="row">
-		<div class="col-sm-6 col-md-4 col-lg-3">
-			<?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+		<div class="col-sm-9 col-md-7 col-lg-5">
+			<?= $withReceiverField ? $form->field($model, 'receiver_id')->widget(ShipmentAddressWidget::class, [
+				'role' => ShipmentAddress::ROLE_RECEIVER,
+				'options' => ['placeholder' => Module::t('postal', 'Choose Receiver')],
+				'addressProperty' => 'receiverAddress',
+			])->hint(
+				Html::a(
+					Module::t('postal', 'Create Receiver Address'),
+					['shipment-address/create']
+				)
+			) : '' ?>
 		</div>
 	</div>
 
 	<div class="row">
-		<div class="col-sm-6 col-md-4 col-lg-3">
-			<?= $form->field($model, 'isSender')->checkbox() ?>
-		</div>
-		<div class="col-sm-6 col-md-4 col-lg-3">
-			<?= $form->field($model, 'isReceiver')->checkbox() ?>
+		<div class="col-sm-6 col-md-5 col-lg-4">
+			<?= ($model->isInScenario() || $model->isUpdateIn())
+				? $form->field($model, 'finished_at')->widget(DateTimeWidget::class)
+				: ''
+			?>
 		</div>
 	</div>
 
-	<?php if ($formWasNull): ?>
-		<div class="form-group">
-			<?= Html::submitButton(Module::t('common', 'Save'), ['class' => 'btn btn-success']) ?>
-		</div>
+	<div class="form-group mt-3">
+		<?= Html::submitButton(Module::t('common', 'Save'), ['class' => 'btn btn-success']) ?>
+	</div>
 
-		<?php ActiveForm::end(); ?>
-	<?php endif; ?>
-
+	<?php ActiveForm::end(); ?>
 
 </div>
--- src/widgets/views/content/_form.php
+++ src/widgets/views/content/_form.php
@@ -16,13 +16,13 @@ use yii\widgets\ActiveForm;
 	]); ?>
 
 	<div class="row">
-		<div class="col-12 col-sm-12 col-md-12 col-lg-12">
+		<div class="col-sm-6 col-md-6 col-lg-6">
 			<?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 		</div>
 	</div>
 
 	<div class="row">
-		<div class="col-6 col-sm-6 col-md-2 col-lg-1">
+		<div class="col-sm-6 col-md-3 col-lg-2">
 			<?= $form->field($model, 'is_active')->checkbox() ?>
 		</div>
 	</div>
