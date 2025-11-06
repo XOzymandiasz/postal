34465b5337c0ec06d54e27974a248eab7cd9f5f5
adamk <adam.klamrowski.ak@gmail.com>
2025-11-06 16:54:25 +0100

    test: add more data for fixtures

--- tests/_data/user.php
+++ tests/_data/user.php
@@ -9,5 +9,15 @@ return [
         'id' => 2,
         'name' => 'Stefan',
         'auth_key' => 'test',
-    ]
+    ],
+	'manager' => [
+		'id' => 3,
+		'name' => 'Katarzyna',
+		'auth_key' => 'manager123',
+	],
+	'worker' => [
+		'id' => 4,
+		'name' => 'Piotr',
+		'auth_key' => 'worker456',
+	],
 ];
