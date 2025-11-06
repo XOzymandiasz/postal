f7125c3a94607b768ce7d7c3685a6fd9a60d205d
adamk <adam.klamrowski.ak@gmail.com>
2025-11-06 14:22:00 +0100

    feat: add condition is not finished to display delete and update buttons

--- src/views/shipment/index.php
+++ src/views/shipment/index.php
@@ -17,7 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="postal-shipment-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+	<h1><?= Html::encode($this->title) ?></h1>
 
 	<p>
 		<?= Html::a(
@@ -43,120 +43,135 @@ $this->params['breadcrumbs'][] = $this->title;
 		) ?>
 	</p>
 
-    <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
+	<?php  echo $this->render('_search', ['model' => $searchModel]); ?>
 
 
-    <?=
-    GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
+	<?=
+	GridView::widget([
+		'dataProvider' => $dataProvider,
+		'filterModel' => $searchModel,
+		'columns' => [
 
-            [
-                'attribute' => 'direction',
-                'filter' => $searchModel::getDirectionsNames(),
-                'label' => Module::t('postal', 'Direction'),
-                'value' => 'directionName',
-            ],
-            [
-                    'attribute' => 'number',
-                'label' => Module::t('postal', 'Number'),
-            ],
-            [
-                'attribute' => 'provider',
-                'value' => 'providerName',
-                'label' => Module::t('postal', 'Provider'),
-                'filter' => $searchModel::getProvidersNames(),
-            ],
-            [
-                'attribute' => 'content_id',
-                'label' => Module::t('postal', 'Content'),
-                'value' => function (Shipment $model) {
-                    return $model->content->name ?? Module::t('common', '(empty)');
-                },
-                'filter' => $searchModel::getContentsNames(),
-            ],
-            [
-                'attribute' => 'creator_id',
-                'value' => 'creator.name',
-                'filter' => $searchModel::getCreatorsNames(),
-                'visible' => $searchModel->isCreatorScenario(),
-            ],
-            //'creator_id',
-            //'guid',
-            //'buffer_id',
-            //'shipment_at',
-            //'api_data',
-            [
-                'attribute' => 'senderName',
-                'label' => Module::t('postal', 'Sender Name'),
-                'value' => function (Shipment $model) {
-                    if ($model->senderAddress !== null) {
-                        return $model->senderAddress->getFullName();
-                    }
-                    else{
-                        return Module::t('postal', 'None');
-                    }
-                }
-            ],
-            [
-                'attribute' => 'senderAddress',
-                'label' => Module::t('postal', 'Sender Address'),
-                'value' => function (Shipment $model) {
-                    if ($model->senderAddress !== null) {
-                        return $model->senderAddress->getFullLocation();
-                    }
-                    else{
-                        return Module::t('postal', 'None');
-                    }
-                }
-            ],
-            [
-                'attribute' => 'receiverName',
-                'label' => Module::t('postal', 'Receiver Name'),
-                'value' => function (Shipment $model) {
-                    if ($model->receiverAddress !== null) {
-                        return $model->receiverAddress->getFullName();
-                    }
-                    else{
-                        return Module::t('postal', 'None');
-                    }
-                }
-            ],
-            [
-                'attribute' => 'receiverAddress',
-                'label' => Module::t('postal', 'Receiver Address'),
-                'value' => function (Shipment $model) {
-                    if ($model->receiverAddress !== null) {
-                        return $model->receiverAddress->getFullLocation();
-                    }
-                    else{
-                        return Module::t('postal', 'None');
-                    }
-                }
-            ],
-            [
-                'attribute' => 'created_at',
-                'label' => Module::t('postal', 'Created At'),
-            ],
-            [
-                'attribute' => 'updated_at',
-                'label' => Module::t('postal', 'Updated At'),
-            ],
-            [
-                'attribute' => 'finished_at',
-                'label' => Module::t('postal', 'Finished At'),
-            ],
-            [
-                'class' => ActionColumn::class,
-                'urlCreator' => function ($action, Shipment $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                }
-            ],
+			[
+				'attribute' => 'direction',
+				'filter' => $searchModel::getDirectionsNames(),
+				'label' => Module::t('postal', 'Direction'),
+				'value' => 'directionName',
+			],
+			[
+				'attribute' => 'number',
+				'label' => Module::t('postal', 'Number'),
+			],
+			[
+				'attribute' => 'provider',
+				'value' => 'providerName',
+				'label' => Module::t('postal', 'Provider'),
+				'filter' => $searchModel::getProvidersNames(),
+			],
+			[
+				'attribute' => 'content_id',
+				'label' => Module::t('postal', 'Content'),
+				'value' => function (Shipment $model) {
+					return $model->content->name ?? Module::t('common', '(empty)');
+				},
+				'filter' => $searchModel::getContentsNames(),
+			],
+			[
+				'attribute' => 'creator_id',
+				'value' => 'creator.name',
+				'filter' => $searchModel::getCreatorsNames(),
+				'visible' => $searchModel->isCreatorScenario(),
+			],
+			//'creator_id',
+			//'guid',
+			//'buffer_id',
+			//'shipment_at',
+			//'api_data',
+			[
+				'attribute' => 'senderName',
+				'label' => Module::t('postal', 'Sender Name'),
+				'value' => function (Shipment $model) {
+					if ($model->senderAddress !== null) {
+						return $model->senderAddress->getFullName();
+					}
+					else{
+						return Module::t('postal', 'None');
+					}
+				}
+			],
+			[
+				'attribute' => 'senderAddress',
+				'label' => Module::t('postal', 'Sender Address'),
+				'value' => function (Shipment $model) {
+					if ($model->senderAddress !== null) {
+						return $model->senderAddress->getFullLocation();
+					}
+					else{
+						return Module::t('postal', 'None');
+					}
+				}
+			],
+			[
+				'attribute' => 'receiverName',
+				'label' => Module::t('postal', 'Receiver Name'),
+				'value' => function (Shipment $model) {
+					if ($model->receiverAddress !== null) {
+						return $model->receiverAddress->getFullName();
+					}
+					else{
+						return Module::t('postal', 'None');
+					}
+				}
+			],
+			[
+				'attribute' => 'receiverAddress',
+				'label' => Module::t('postal', 'Receiver Address'),
+				'value' => function (Shipment $model) {
+					if ($model->receiverAddress !== null) {
+						return $model->receiverAddress->getFullLocation();
+					}
+					else{
+						return Module::t('postal', 'None');
+					}
+				}
+			],
+			[
+				'attribute' => 'created_at',
+				'label' => Module::t('postal', 'Created At'),
+			],
+			[
+				'attribute' => 'updated_at',
+				'label' => Module::t('postal', 'Updated At'),
+			],
+			[
+				'attribute' => 'finished_at',
+				'label' => Module::t('postal', 'Finished At'),
+			],
+			[
+				'class' => ActionColumn::class,
+				'template' => '{view} {update} {delete}',
+				'urlCreator' => function ($action, Shipment $model, $key, $index, $column) {
+					return Url::toRoute([$action, 'id' => $model->id]);
+				},
+				'buttons' => [
+					'update' => function (string $url, Shipment $model) {
+						return !$model->isFinished()
+							? Html::a('<i class="fa fa-pencil"></i>', $url)
+							: '';
+					},
+					'delete' => function (string $url, Shipment $model) {
+						return !$model->isFinished()
+							? Html::a('<i class="fa fa-trash"></i>', $url, [
+								'data-method' => 'POST',
+								'data-confirm' => Yii::t('common','Are you sure you want to delete this item?')])
+							: '';
+					}
+				]
+			],
 
 
-        ],
-    ]); ?>
+		],
+	]); ?>
 
 
 </div>
