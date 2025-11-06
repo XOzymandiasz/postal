79204d5add4f29024d49451e461d90f19ac146eb
adamk <adam.klamrowski.ak@gmail.com>
2025-11-06 14:22:22 +0100

    style: delete whitespaces

--- src/views/shipment/view.php
+++ src/views/shipment/view.php
@@ -9,51 +9,55 @@ use yii\widgets\DetailView;
 /** @var Shipment $model */
 
 $this->title = Module::t('postal','Shipment {content}', [
-		'content' => $model->content->name,
-	]);
+	'content' => $model->content->name,
+]);
 $this->params['breadcrumbs'][] = ['label' => Module::t('common', 'Postal Shipments'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="postal-shipment-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+	<h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a(Module::t('common', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Module::t('common', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Module::t('poczta-polska', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+	<p>
+		<?= !$model->isFinished()
+			? Html::a(Module::t('common', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary'])
+			: ''
+		?>
+		<?= !$model->isFinished()
+			? Html::a(Module::t('common', 'Delete'), ['delete', 'id' => $model->id], [
+				'class' => 'btn btn-danger',
+				'data' => [
+					'confirm' => Module::t('common', 'Are you sure you want to delete this item?'),
+					'method' => 'post',
+				],
+			]) : ''?>
+	</p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
+	<?= DetailView::widget([
+		'model' => $model,
+		'attributes' => [
 			[
 				'attribute' => 'direction',
 				'value' => function(Shipment $model){
 					return $model->getDirectionName();
 				}
 			],
-            'number',
+			'number',
 			[
 				'attribute' => 'provider',
 				'value' => function(Shipment $model){
 					return $model->getProviderName();
 				}
 			],
-            //'content_id',
-            //'creator_id',
-            'created_at',
-            'updated_at',
-            //'guid',
-            'finished_at',
-            'shipment_at',
-        ],
-    ]) ?>
+			//'content_id',
+			//'creator_id',
+			'created_at',
+			'updated_at',
+			//'guid',
+			'finished_at',
+			'shipment_at',
+		],
+	]) ?>
 
 </div>
